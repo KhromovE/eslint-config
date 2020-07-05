9be9bbd9bf246e1d02b7d545dfaab794beea42fa
Egor <khromove@gmail.com>
2020-07-05 16:07:07 +0700

    update packages versions

--- packages/typescript/.eslintrc.js
+++ packages/typescript/.eslintrc.js
@@ -1,41 +1,41 @@
 module.exports = {
+  root: true,
   env: {
-    es6: true,
-    node: true
+    es2020: true,
   },
   extends: [
-    "airbnb-base",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
+    'airbnb-base',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module"
+    ecmaVersion: 11,
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   rules: {
-    "import/prefer-default-export": "off",
-    "import/no-default-export": "error",
-    "no-unused-expressions": [
-      "warn",
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+    'no-unused-expressions': [
+      'warn',
       {
         allowShortCircuit: true,
-        allowTernary: true
-      }
+        allowTernary: true,
+      },
     ],
-    "@typescript-eslint/prefer-interface": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+    '@typescript-eslint/prefer-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-          ts: "never",
-          tsx: "never"
-      }
-    ]
-  }
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
 }
